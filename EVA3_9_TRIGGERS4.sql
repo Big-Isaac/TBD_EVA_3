82a4002baf2821473b455d2c2ccc9189ae0b99bc
Big-Isaac <168494899+Big-Isaac@users.noreply.github.com>
2024-11-25 21:23:32 -0600

    Add files via upload

---
+++ EVA3_9_TRIGGERS4.sql
@@ -0,0 +1,4 @@
+CREATE DEFINER=`root`@`localhost` TRIGGER `productos_AFTER_INSERT` AFTER INSERT ON `productos` FOR EACH ROW BEGIN
+	set @contador = @contador + 1;
+END
+
---
+++ EVA3_9_TRIGGERS5.sql
@@ -0,0 +1,10 @@
+delimiter $$
+create function generar_rfc2()
+returns varchar(15) deterministic
+begin
+	return 'XXXX11111000';
+    
+end$$
+
+
+delimiter ;
